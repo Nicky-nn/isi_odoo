6eb98dca36cb327d8dc0846bd1756476955d1d83
nick <nickynn07@gmail.com>
2024-12-16 19:19:32 -0400

    Actualizar vista del wizard para cambiar sucursal y punto de venta: Renombrar campos y mejorar la estructura del menú

--- models/sucursal_punto_venta.py
+++ models/sucursal_punto_venta.py
@@ -1,148 +1,39 @@
 from odoo import models, fields, api
 import requests
 
-
 class SucursalPuntoVentaWizard(models.TransientModel):
-    # State: Desarrollo Pruebas Alpha
-    # Recordatorio: Agregar Wizar o modal personalizado para cambiar la sucursal y punto de venta activos y la Sinconización de la base de datos.
-
-    # Clase: SucursalPuntoVentaWizard
-    # - Atributos:
-    #   - sucursal_actual: Campo de texto que almacena la sucursal actual seleccionada.
-    #   - punto_venta_actual: Campo de texto que almacena el punto de venta actual seleccionado.
-    #   - sucursales_html: Campo HTML que almacena la información de las sucursales y puntos de venta.
-    #   - sucursal_id: Campo de selección para almacenar la nueva sucursal.
-    #   - punto_venta_id: Campo de selección para almacenar el nuevo punto de venta.
-    #
-    # - Métodos:
-    #   - default_get(fields_list): Obtiene los valores por defecto de los campos.
-    #   - _compute_actual_sucursal_punto_venta(): Calcula la sucursal y punto de venta actual seleccionados.
-    #   - _get_sucursales_html(): Obtiene la información de las sucursales y puntos de venta en formato HTML.
-    #   - _onchange_sucursal_id(): Actualiza el campo de selección de punto de venta basado en la sucursal seleccionada.
-    #   - action_change_sucursal_punto_venta(): Cambia la sucursal y punto de venta activos.
-    #   - get_token_and_url(): Obtiene el token y la URL de la API de la base de datos.
-    #   - get_sucursales_puntos_venta(): Obtiene las sucursales y puntos de venta de la API.
-    #   - cambiar_sucursal_punto_venta_activo(codigo_sucursal, codigo_punto_venta): Cambia la sucursal y punto de venta activos.
-    #   - actualizar_bd_con_nuevos_datos(sucursal_codigo, punto_venta_codigo): Actualiza la base de datos con los nuevos datos.
-
     _name = 'sucursal.punto.venta.wizard'
-    _description = 'Wizard para cambiar Sucursal y Punto de Venta'
+    _description = 'Asistente para cambiar Sucursal y Punto de Venta'
 
-    sucursal_actual = fields.Char(
-        string="Sucursal Actual", compute="_compute_actual_sucursal_punto_venta")
-    punto_venta_actual = fields.Char(
-        string="Punto de Venta Actual", compute="_compute_actual_sucursal_punto_venta")
-    sucursales_html = fields.Html(
-        string="Sucursales y Puntos de Venta", readonly=True)
+    sucursal_actual = fields.Char(string="Sucursal Actual", readonly=True)
+    punto_venta_actual = fields.Char(string="Punto de Venta Actual", readonly=True)
+    nueva_sucursal = fields.Selection(selection='_get_sucursales', string="Código Nueva Sucursal")
+    nuevo_punto_venta = fields.Selection(selection='_get_puntos_venta', string="Código Nuevo Punto de Venta")
 
-    sucursal_id = fields.Many2one('sucursal.info', string="Nueva Sucursal")
-    punto_venta_id = fields.Many2one(
-        'punto.venta.info', string="Nuevo Punto de Venta")
-
-    @api.model
-    def default_get(self, fields_list):
-        res = super(SucursalPuntoVentaWizard, self).default_get(fields_list)
-        if 'sucursales_html' in fields_list:
-            res['sucursales_html'] = self._get_sucursales_html()
-
-        sucursales = self.get_sucursales_puntos_venta()
-        self.env['sucursal.info'].search([]).unlink()
-        self.env['punto.venta.info'].search([]).unlink()
-
-        for sucursal in sucursales:
-            sucursal_record = self.env['sucursal.info'].create({
-                'codigo': sucursal['codigo'],
-                'nombre': f"{sucursal['codigo']} - {sucursal['direccion']}"
-            })
-            for punto_venta in sucursal['puntosVenta']:
-                self.env['punto.venta.info'].create({
-                    'codigo': punto_venta['codigo'],
-                    'nombre': f"{punto_venta['codigo']} - {punto_venta['nombre']}",
-                    'sucursal_id': sucursal_record.id
-                })
-        return res
-
-    @api.depends()
-    def _compute_actual_sucursal_punto_venta(self):
-        for record in self:
-            config = self.env['isi_pass_config'].search(
-                [('user_id', '=', self.env.user.id)], limit=1)
-            record.sucursal_actual = f"{
-                config.sucursal_codigo} - {config.sucursal_direccion}"
-            record.punto_venta_actual = f"{
-                config.punto_venta_codigo} - {config.punto_venta_nombre}"
-
-    def _get_sucursales_html(self):
-        sucursales = self.get_sucursales_puntos_venta()
-        html = "<div class='container'>"
-        for sucursal in sucursales:
-            html += f"""
-                <div class="card mb-3">
-                    <div class="card-header bg-primary text-white">
-                        <h5 class="mb-0">SUCURSAL {sucursal['codigo']}</h5>
-                    </div>
-                    <div class="card-body">
-                        <p><strong>Departamento:</strong> {sucursal['departamento']['departamento']}</p>
-                        <p><strong>Municipio:</strong> {sucursal['municipio']}</p>
-                        <p><strong>Dirección:</strong> {sucursal['direccion']}</p>
-                        <p><strong>Teléfono:</strong> {sucursal['telefono']}</p>
-                        <h6 class="mt-4">Puntos de Venta:</h6>
-                        <div class="list-group">
-            """
-            for punto_venta in sucursal['puntosVenta']:
-                html += f"""
-                    <div class="list-group-item">
-                        <h6 class="mb-1">PUNTO DE VENTA {punto_venta['codigo']}</h6>
-                        <p class="mb-1"><strong>Nombre:</strong> {punto_venta['nombre']}</p>
-                        <p class="mb-1"><strong>Descripción:</strong> {punto_venta['descripcion']}</p>
-                    </div>
-                """
-            html += """
-                        </div>
-                    </div>
-                </div>
-            """
-        html += "</div>"
-        return html
-
-    @api.onchange('sucursal_id')
-    def _onchange_sucursal_id(self):
-        self.punto_venta_id = False
-        return {'domain': {'punto_venta_id': [('sucursal_id', '=', self.sucursal_id.id)]}}
-
-    def action_change_sucursal_punto_venta(self):
-        self.ensure_one()
-        if self.sucursal_id and self.punto_venta_id:
-            result = self.cambiar_sucursal_punto_venta_activo(
-                self.sucursal_id.codigo, self.punto_venta_id.codigo)
-            if result:
-                self.actualizar_bd_con_nuevos_datos(
-                    self.sucursal_id.codigo, self.punto_venta_id.codigo)
-                return {'type': 'ir.actions.client', 'tag': 'reload'}
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'title': 'Error',
-                'message': 'No se pudo cambiar la sucursal y punto de venta.',
-                'type': 'warning',
-            }
-        }
-
-    @api.model
-    def get_token_and_url(self):
-        current_user_id = self.env.user.id
+    def _obtener_token_y_url(self):
         self.env.cr.execute("""
             SELECT token, api_url FROM isi_pass_config
-            WHERE user_id = %s
-            LIMIT 1
-        """, (current_user_id,))
+            WHERE user_id = %s LIMIT 1
+        """, (self.env.user.id,))
         result = self.env.cr.fetchone()
         return result if result else (None, None)
 
     @api.model
+    def _get_sucursales(self):
+        sucursales = self.get_sucursales_puntos_venta()
+        return [(str(suc['codigo']), f"{suc['direccion']} (Código: {suc['codigo']})") for suc in sucursales]
+
+    @api.model
+    def _get_puntos_venta(self):
+        sucursales = self.get_sucursales_puntos_venta()
+        puntos = []
+        for suc in sucursales:
+            for punto in suc.get('puntosVenta', []):
+                puntos.append((str(punto['codigo']), f"{punto['nombre']} (Código: {punto['codigo']})"))
+        return puntos
+
     def get_sucursales_puntos_venta(self):
-        token, api_url = self.get_token_and_url()
+        token, api_url = self._obtener_token_y_url()
         if not token or not api_url:
             return []
 
@@ -156,18 +47,10 @@ class SucursalPuntoVentaWizard(models.TransientModel):
             usuarioRestriccion {
                 sucursales {
                     codigo
-                    telefono
                     direccion
-                    municipio
-                    departamento {
-                        departamento
-                    }
+                    telefono
                     puntosVenta {
                         codigo
-                        tipoPuntoVenta {
-                            codigoClasificador
-                            descripcion
-                        }
                         nombre
                         descripcion
                     }
@@ -177,8 +60,7 @@ class SucursalPuntoVentaWizard(models.TransientModel):
         """
 
         try:
-            response = requests.post(
-                api_url, headers=headers, json={'query': query})
+            response = requests.post(api_url, headers=headers, json={'query': query})
             if response.status_code == 200:
                 data = response.json()
                 return data.get('data', {}).get('usuarioRestriccion', {}).get('sucursales', [])
@@ -188,9 +70,9 @@ class SucursalPuntoVentaWizard(models.TransientModel):
             return []
 
     def cambiar_sucursal_punto_venta_activo(self, codigo_sucursal, codigo_punto_venta):
-        token, api_url = self.get_token_and_url()
+        token, api_url = self._obtener_token_y_url()
         if not token or not api_url:
-            return None
+            return False
 
         headers = {
             'Content-Type': 'application/json',
@@ -221,76 +103,70 @@ class SucursalPuntoVentaWizard(models.TransientModel):
             response = requests.post(api_url, headers=headers, json={
                                      'query': mutation, 'variables': variables})
             if response.status_code == 200:
-                return response.json().get('data', {}).get('usuarioCambiarSucursalPuntoVentaActivo')
+                return response.json().get('data', {}).get('usuarioCambiarSucursalPuntoVentaActivo') is not None
             else:
-                return None
+                return False
         except requests.exceptions.RequestException:
-            return None
+            return False
 
-    def actualizar_bd_con_nuevos_datos(self, sucursal_codigo, punto_venta_codigo):
-        sucursales = self.get_sucursales_puntos_venta()
-        sucursal = next(
-            (s for s in sucursales if s['codigo'] == sucursal_codigo), None)
-        if sucursal:
-            punto_venta = next(
-                (pv for pv in sucursal['puntosVenta'] if pv['codigo'] == punto_venta_codigo), None)
-            if punto_venta:
+    def action_cambiar_sucursal_punto_venta(self):
+        exito = self.cambiar_sucursal_punto_venta_activo(
+            self.nueva_sucursal, self.nuevo_punto_venta)
+        if exito:
+            # Obtener detalles de la nueva sucursal y punto de venta
+            sucursales = self.get_sucursales_puntos_venta()
+            nueva_sucursal_info = next(
+                (suc for suc in sucursales if str(suc['codigo']) == self.nueva_sucursal), None)
+            nuevo_punto_venta_info = None
+            if nueva_sucursal_info:
+                nuevo_punto_venta_info = next(
+                    (punto for punto in nueva_sucursal_info.get('puntosVenta', []) if str(punto['codigo']) == self.nuevo_punto_venta), None)
+
+            # Actualizar la tabla isi_pass_config directamente mediante SQL
+            if nueva_sucursal_info and nuevo_punto_venta_info:
                 self.env.cr.execute("""
                     UPDATE isi_pass_config
-                    SET sucursal_codigo = %s,
+                    SET
+                        sucursal_codigo = %s,
                         sucursal_direccion = %s,
                         sucursal_telefono = %s,
-                        departamento_codigo = %s,
-                        departamento_nombre = %s,
                         punto_venta_codigo = %s,
                         punto_venta_nombre = %s,
-                        punto_venta_descripcion = %s
+                        punto_venta_descripcion = %s,
+                        write_uid = %s,
+                        write_date = NOW()
                     WHERE user_id = %s
                 """, (
-                    sucursal['codigo'],
-                    sucursal['direccion'],
-                    sucursal['telefono'],
-                    sucursal['departamento'].get('codigo', ''),
-                    sucursal['departamento'].get('departamento', ''),
-                    punto_venta['codigo'],
-                    punto_venta['nombre'],
-                    punto_venta['descripcion'],
+                    self.nueva_sucursal,
+                    nueva_sucursal_info.get('direccion'),
+                    nueva_sucursal_info.get('telefono'),
+                    self.nuevo_punto_venta,
+                    nuevo_punto_venta_info.get('nombre'),
+                    nuevo_punto_venta_info.get('descripcion'),
+                    self.env.uid,
                     self.env.user.id
                 ))
-                self.env.cr.commit()
 
-
-class SucursalInfo(models.TransientModel):
-
-    # Clase: SucursalInfo
-    # - Atributos:
-    #   - codigo: Campo entero que almacena el código de la sucursal.
-    #   - nombre: Campo de texto que almacena el nombre de la sucursal.
-    #
-    # - Métodos:
-    #   - No tiene métodos.
-
-    _name = 'sucursal.info'
-    _description = 'Información de Sucursal'
-
-    codigo = fields.Integer(string="Código")
-    nombre = fields.Char(string="Nombre")
-
-
-class PuntoVentaInfo(models.TransientModel):
-
-    # Clase: PuntoVentaInfo
-    # - Atributos:
-    #   - codigo: Campo entero que almacena el código del punto de venta.
-    #   - nombre: Campo de texto que almacena el nombre del punto de venta.
-    #   - sucursal_id: Campo de selección que almacena la sucursal a la que pertenece el punto de venta.
-    #
-    # - Métodos:
-    #   - No tiene métodos.
-
-    _name = 'punto.venta.info'
-    _description = 'Información de Punto de Venta'
-
-    codigo = fields.Integer(string="Código")
-    nombre = fields.Char(string="Nombre")
-    sucursal_id = fields.Many2one('sucursal.info', string="Sucursal")
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Éxito',
+                    'message': 'Sucursal y Punto de Venta cambiados exitosamente.',
+                    'type': 'success',
+                    'sticky': False,
+                }
+            }, {
+                'type': 'ir.actions.act_window_close'
+            }
+        else:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Error',
+                    'message': 'No se pudo cambiar la Sucursal y Punto de Venta.',
+                    'type': 'danger',
+                    'sticky': False,
+                }
+            }

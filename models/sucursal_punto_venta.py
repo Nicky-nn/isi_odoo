cb6c06909213e7e36d4e911699df994e341db1a3
nick <nickynn07@gmail.com>
2024-12-17 13:47:53 -0400

    Actualizar modelo SucursalPuntoVentaWizard: Mejorar la obtención de valores por defecto y formatear campos

--- models/sucursal_punto_venta.py
+++ models/sucursal_punto_venta.py
@@ -1,14 +1,18 @@
 from odoo import models, fields, api
 import requests
 
+
 class SucursalPuntoVentaWizard(models.TransientModel):
     _name = 'sucursal.punto.venta.wizard'
     _description = 'Asistente para cambiar Sucursal y Punto de Venta'
 
     sucursal_actual = fields.Char(string="Sucursal Actual", readonly=True)
-    punto_venta_actual = fields.Char(string="Punto de Venta Actual", readonly=True)
-    nueva_sucursal = fields.Selection(selection='_get_sucursales', string="Código Nueva Sucursal")
-    nuevo_punto_venta = fields.Selection(selection='_get_puntos_venta', string="Código Nuevo Punto de Venta")
+    punto_venta_actual = fields.Char(
+        string="Punto de Venta Actual", readonly=True)
+    nueva_sucursal = fields.Selection(
+        selection='_get_sucursales', string="Código Sucursal")
+    nuevo_punto_venta = fields.Selection(
+        selection='_get_puntos_venta', string="Código Punto de Venta")
 
     def _obtener_token_y_url(self):
         self.env.cr.execute("""
@@ -29,8 +33,39 @@ class SucursalPuntoVentaWizard(models.TransientModel):
         puntos = []
         for suc in sucursales:
             for punto in suc.get('puntosVenta', []):
-                puntos.append((str(punto['codigo']), f"{punto['nombre']} (Código: {punto['codigo']})"))
+                puntos.append((str(punto['codigo']), f"{
+                              punto['nombre']} (Código: {punto['codigo']})"))
         return puntos
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(SucursalPuntoVentaWizard, self).default_get(fields_list)
+
+        # Obtener los valores actuales desde isi_pass_config
+        self.env.cr.execute("""
+            SELECT sucursal_codigo, sucursal_direccion, punto_venta_codigo, punto_venta_nombre 
+            FROM isi_pass_config 
+            WHERE user_id = %s LIMIT 1
+        """, (self.env.user.id,))
+        current_values = self.env.cr.fetchone()
+
+        if current_values:
+            sucursal_codigo, sucursal_direccion, punto_venta_codigo, punto_venta_nombre = current_values
+            res.update({
+                'sucursal_actual': f"{sucursal_direccion} (Código: {sucursal_codigo})" if sucursal_direccion and sucursal_codigo else '',
+                'punto_venta_actual': f"{punto_venta_nombre} (Código: {punto_venta_codigo})" if punto_venta_nombre and punto_venta_codigo else '',
+                'nueva_sucursal': str(sucursal_codigo) if sucursal_codigo else '',
+                'nuevo_punto_venta': str(punto_venta_codigo) if punto_venta_codigo else '',
+            })
+        else:
+            res.update({
+                'sucursal_actual': '',
+                'punto_venta_actual': '',
+                'nueva_sucursal': '',
+                'nuevo_punto_venta': '',
+            })
+
+        return res
 
     def get_sucursales_puntos_venta(self):
         token, api_url = self._obtener_token_y_url()
@@ -60,7 +95,8 @@ class SucursalPuntoVentaWizard(models.TransientModel):
         """
 
         try:
-            response = requests.post(api_url, headers=headers, json={'query': query})
+            response = requests.post(
+                api_url, headers=headers, json={'query': query})
             if response.status_code == 200:
                 data = response.json()
                 return data.get('data', {}).get('usuarioRestriccion', {}).get('sucursales', [])
@@ -155,10 +191,13 @@ class SucursalPuntoVentaWizard(models.TransientModel):
                     'message': 'Sucursal y Punto de Venta cambiados exitosamente.',
                     'type': 'success',
                     'sticky': False,
+                    'next': {
+                        'type': 'ir.actions.client',
+                        'tag': 'reload',  # Acción para recargar la página
+                    }
                 }
-            }, {
-                'type': 'ir.actions.act_window_close'
             }
+
         else:
             return {
                 'type': 'ir.actions.client',
